e30de0c62d462bcecfa58997b1a03048fa19e260
Megan <megan.miller@smoldev.dev>
2020-02-11 17:45:22 -0600

    Created the player class

--- src/player.py
+++ src/player.py
@@ -1,2 +1,12 @@
+from colorama import Fore, Style
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+
+class Player:
+    def __init__(self, name, room):
+        self.name = name
+        self.room = room
+
+    def __str__(self):
+        return f"{Fore.GREEN}{self.name}{Style.RESET_ALL} is in {Fore.YELLOW}{self.room}{Style.RESET_ALL}"
