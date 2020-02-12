67d4eb979bfce484d9e4db6c83365ce6c60ec308
Megan <megan.miller@smoldev.dev>
2020-02-12 17:47:02 -0600

    Aggregating Item onto room.

--- src/room.py
+++ src/room.py
@@ -1,4 +1,5 @@
 import textwrap
+from item import Item
 # Implement a class to hold room information. This should have name and
 # description attributes.
 
@@ -6,19 +7,29 @@ wrapper = textwrap.TextWrapper(width=50)
 
 
 class Room:
-    def __init__(self, name, description, n_to=None, s_to=None, e_to=None, 
-                 w_to=None):
+    def __init__(self, name, description, items=[], n=None, s=None, e=None, 
+                 w=None):
         self.name = name
         self.description = description
-        self.n_to = n_to
-        self.s_to = s_to
-        self.e_to = e_to
-        self.w_to = w_to
+        self.n = n
+        self.s = s
+        self.e = e
+        self.w = w
+        self.items = items
+
+    description_str = ""
 
     def desc(self):
         wrapper = textwrap.TextWrapper(width=70)
         desc = wrapper.fill(text=self.description)
         return f"{desc}"
 
+    def show_items(self):
+        if len(self.items):
+            for i in self.items:
+                global description_str f"{i.name}: {i.description}\n"
+        else:
+            return None
+
     def __str__(self):
-        return f"{self.name}:\n{Room.desc(self)}"
+        return f"{self.name}:\n{Room.desc(self)}\nItems here: {description_str}"
